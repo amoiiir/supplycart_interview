a1b27d836f4d1529e73f4247bf5f19e0791eac2b
Amir Hamzah <mahamzah535@gmail.com>
2024-02-10 01:56:42 +0800

    Added guest page

--- backend/src/components/GuestLayout.vue
+++ backend/src/components/GuestLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
@@ -38,8 +38,15 @@
         </p>
       </div>
     </div>
-  </template>
+</template>
+
+<script>
+export default {
+  name: 'GuestLayout',
+  // Your JavaScript logic goes here
+}
+</script>
 
 <style scoped>
-
+/* Your CSS styles go here */
 </style>
