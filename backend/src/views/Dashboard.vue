782c2be03806cb07bdf4b63674e160c7f6a06e70
Amir Hamzah <mahamzah535@gmail.com>
2024-02-09 11:27:46 +0800

    first try for vue form

---
+++ backend/src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Dashboard</h1>
+  </template>
+
+  <script>
+  export default {
+    // Your JavaScript code here
+    name : "Dashboard"
+  }
+  </script>
+
+  <style scoped>
+  /* Your CSS code here */
+  </style>
