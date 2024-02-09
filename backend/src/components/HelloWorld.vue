9935db06c5d9f766f28b3923d791b81a7daee22a
Amir Hamzah <mahamzah535@gmail.com>
2024-02-09 09:33:48 +0800

    test commit

--- backend/src/components/HelloWorld.vue
+++ backend/src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit my name here is borat, hehe, hello, please save me under the water
+      Edit my name here is borat, hehe, hello, please save me under the water, help me please master
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
